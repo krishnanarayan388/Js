8edd5e70c41234f3f40140909f4ebae7918a788e
Krishna Narayan <krishnanarayan388@gmail.com>
2024-11-07 21:46:04 +0530

    com

---
+++ math.js
@@ -0,0 +1,31 @@
+// const score=100
+// console.log(score);
+
+// const balance= new Number(1200)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// // +++++++++++++++++++++++++++MATHS++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(5.2));
+// console.log(Math.sqrt(25));
+// console.log(Math.pow(5,2));
+// console.log(Math.min(12,223,45));
+
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log((Math.random()*10)+1);
+
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+
--- str.js
+++ str.js
@@ -1,3 +1,4 @@
 const name="Krishna"
 const anotherName="Rahul"
 console.log(`Hello my name is ${name} and my another name is ${anotherName}`);
+console.log(name.toUpperCase())
